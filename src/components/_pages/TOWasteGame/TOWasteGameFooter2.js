a15b2b1a9a1db7258a52b8a5e42238e8add8a98b
Daniel Rodrigues <danielrodrigues@MacBook-Pro-de-Carla.local>
2019-04-07 13:15:18 -0400

    Footer is now responsive

--- src/components/_pages/TOWasteGame/TOWasteGameFooter2.js
+++ src/components/_pages/TOWasteGame/TOWasteGameFooter2.js
@@ -7,11 +7,11 @@ class TOWasteGameFooter2 extends Component {
             <div className="col-md-6 text-center p-3 border-right border-left">
                 <h3 className="logo-font">Contact</h3>
                 <ul className="list-unstyled text-left">
-                    <h4>
+                    <h5>
                         <li><a href="https://github.com/DrodBR/" className="text-white" target="_blank" rel="noopener noreferrer"><i class="fab fa-github-square p-2 pr-3"></i>/DrodBR</a></li>
                         <li><a href="https://www.linkedin.com/in/danielfgrodrigues/" className="text-white" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin p-2 pr-3"></i>/danielfgrodrigues/</a></li>
                         <li><i class="fas fa-envelope-square p-2 pr-3"></i>daniel.fernando@msn.com</li>
-                    </h4>
+                    </h5>
                 </ul>
             </div>
         )
