37b3406bbde130b73c441394057fd4f72d13e776
Daniel Rodrigues <danielrodrigues@MacBook-Pro-de-Carla.local>
2019-04-09 18:27:28 -0400

    Navbar improved.

--- src/components/_layouts/Navbar.js
+++ src/components/_layouts/Navbar.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
- 
+
 class Navbar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg border-bottom navbarcustom py-3">
-                <div class="navbar-brand logo-font"><h1><i class="fas fa-city"></i> TOWaste Game<h6>by Daniel Rodrigues</h6></h1></div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <nav className="navbar navbar-expand-lg border-bottom navbarcustom py-3">
+                <div className="navbar-brand logo-font">
+                    <h1><i class="fas fa-city"></i> TOWaste Game</h1>
+                    <h6>by Daniel Rodrigues</h6>
+                </div>
             </nav>
         );
     }
